54b57b461f5e980b5149dae81f10e0bc05fb8f42
GERARD2619-17 <gera121134@gmail.com>
2023-11-01 14:12:37 -0600

    prueba

--- logearme.js
+++ logearme.js
@@ -8,7 +8,7 @@ document.addEventListener('DOMContentLoaded', () => {
         const password = document.getElementById('password').value;
 
         try {
-            const response = await fetch('http://localhost:1337/api/auth/local', {
+            const response = await fetch('https://e6ce-190-62-3-87.ngrok-free.app/api/auth/local', {
                 method: 'POST',
                 headers: {
                     'Content-Type': 'application/json',
